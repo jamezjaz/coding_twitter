535e10b09223624a58943ec832afe8d034d879ae
Jamezjaz <jamezjaz@gmail.com>
2020-08-13 12:42:47 -0500

    Adds validations and installs RSpec & Capybara  gems

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'font-awesome-sass', '~> 5.12.0'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "database_cleaner"
+  gem "rspec-rails"
 end
 
 group :development do
@@ -55,7 +57,8 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 2.15'
+  gem 'capybara'
+  gem 'rspec'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,21 @@
 class User < ApplicationRecord
-    has_many :opinions, foreign_key: 'author_id'
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  validates :fullname, presence: true, length: { minimum: 5, maximum: 20 }
+  
+  has_many :opinions, foreign_key: 'author_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-    has_many :followings, foreign_key: 'follower_id'
-    has_many :following_users, through: :followings, source: :inverse_user
+  has_many :followings, foreign_key: 'follower_id'
+  has_many :following_users, through: :followings, source: :inverse_user
 
-    has_many :followers, foreign_key: 'followed_id', class_name: 'Following'
-    has_many :follower_users, through: :followers, source: :user
+  has_many :followers, foreign_key: 'followed_id', class_name: 'Following'
+  has_many :follower_users, through: :followers, source: :user
 
-    has_one_attached :photo
-    has_one_attached :coverimage
+  has_one_attached :photo
+  has_one_attached :coverimage
 
-    def follower?(current_user, user)
-      current_user.following_users.ids.include?(user)
-    end
+  def follower?(current_user, user)
+    current_user.following_users.ids.include?(user)
+  end
 end
