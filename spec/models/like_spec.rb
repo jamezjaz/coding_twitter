ed46c6cb4bd3069057191890267fabe5d0b0b568
Jamezjaz <jamezjaz@gmail.com>
2020-08-13 18:52:18 -0500

    Adds association test for Like model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.describe 'Like' do
+  let(:user) { User.create(username: 'James', fullname: 'james james') }
+  let(:tweet) { user.opinions.build(id: 1, text: 'It cannot be better than this').save }
+  let(:like) { user.likes.build(opinion_id: 1).save }
+
+  describe 'Association amongst users and likes' do
+
+    it 'should be valid if association between user and like is valid' do
+      user
+      tweet
+      like
+      expect(user.likes.first.opinion_id).to eql(1)
+    end
+  end
+end
