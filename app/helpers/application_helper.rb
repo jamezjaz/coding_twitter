4ba68f68980be6c2161e908bd2b847e02761a3ed
Jamezjaz <jamezjaz@gmail.com>
2020-08-16 15:02:24 -0500

    Implements default gravatar image for users without images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,30 @@ module ApplicationHelper
       link_to "<i class='text-success far fa-thumbs-up fa-2x'></i>".html_safe, likes_path(opinion_id: opinion), method: :post
     end
   end
+
+  def image_check(user_image)
+    if user_image.attached?
+      link_to image_tag(user_image, class: 'img-fluid rounded'), user_path(current_user)
+    else
+      link_to image_tag('http://www.gravatar.com/avatar', class: 'w-100 h-100 rounded'), user_path(current_user)
+    end
+  end
+
+  def image_check_right(user_image)
+    if user_image.attached?
+      link_to image_tag(user_image, class: 'img-fluid rounded-circle'), user_path(current_user)
+    else
+      link_to image_tag('http://www.gravatar.com/avatar', class: 'w-100 h-100 rounded-circle'), user_path(current_user)
+    end
+  end
+
+  def image_check_cover(user_image)
+    if user_image.attached?
+      link_to image_tag(user_image, class: 'cover-image'), user_path(current_user)
+    else
+      link_to image_tag('http://www.gravatar.com/avatar', class: 'w-100 h-100'), user_path(current_user)
+    end
+  end
 end
 
 # rubocop:enable Layout/LineLength
