9562c29da5018bb5c11506890250341a0336538d
Jamezjaz <jamezjaz@gmail.com>
2020-08-17 20:10:44 -0500

    Correct rubocop errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,40 +1,44 @@
+# rubocop:disable Layout/LineLength
+
 module UsersHelper
   def follower_check(current_user, user)
-    if user.id != current_user.id
-      concat(content_tag(:div, class: 'd-flex border-top') do
-        concat(content_tag(:div, class: 'pt-2 pr-2') do
-          image_check_right(user.photo)
-        end)
-        concat(content_tag(:div, class: 'follower-name') do
-          concat(content_tag(:h4, (link_to user.fullname, user_path(user.id))))
-        end)
+    return unless user.id != current_user.id
+
+    concat(content_tag(:div, class: 'd-flex border-top') do
+      concat(content_tag(:div, class: 'pt-2 pr-2') do
+        image_check_right(user.photo)
       end)
-    end
+      concat(content_tag(:div, class: 'follower-name') do
+        concat(content_tag(:h4, (link_to user.fullname, user_path(user.id))))
+      end)
+    end)
   end
 
   def follow_unfollow_links(current_user, user)
-    if user.id != current_user.id
-      if user.follower?(current_user, user.id)
-        link_to "<i class='text-warning fas fa-minus-circle fa-2x'></i>".html_safe, following_path(followed_id: user.id), method: :delete
-      else
-        link_to "<i class='text-success fas fa-plus-circle fa-2x'></i>".html_safe, followings_path(followed_id: user.id), method: :post
-      end
+    return unless user.id != current_user.id
+
+    if user.follower?(current_user, user.id)
+      link_to "<i class='text-warning fas fa-minus-circle fa-2x'></i>".html_safe, following_path(followed_id: user.id), method: :delete
+    else
+      link_to "<i class='text-success fas fa-plus-circle fa-2x'></i>".html_safe, followings_path(followed_id: user.id), method: :post
     end
   end
 
   def followed_by(current_user, user)
-    if user.id != current_user.id
-      if !user.follower?(current_user, user.id)       
-          concat(content_tag(:div, class: 'd-flex border-top') do
-            concat(content_tag(:div, class: '') do
-              image_check_right(user.photo)
-            end)
-            concat(content_tag(:div, class: 'follower-name') do
-              concat(content_tag(:h4, (link_to user.fullname, user_path(user.id))))
-            end)
-          end)
-          link_to "<i class='text-success fas fa-plus-circle fa-2x d-flex justify-content-end'></i>".html_safe, followings_path(followed_id: user.id), method: :post
-      end
-    end
+    next unless user.id != current_user.id
+
+    return unless !user.follower?(current_user, user.id)
+
+    concat(content_tag(:div, class: 'd-flex border-top') do
+      concat(content_tag(:div, class: '') do
+        image_check_right(user.photo)
+      end)
+      concat(content_tag(:div, class: 'follower-name') do
+        concat(content_tag(:h4, (link_to user.fullname, user_path(user.id))))
+      end)
+    end)
+    link_to "<i class='text-success fas fa-plus-circle fa-2x d-flex justify-content-end'></i>".html_safe, followings_path(followed_id: user.id), method: :post
   end
 end
+
+# rubocop:enable Layout/LineLength
