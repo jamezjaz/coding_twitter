a0e618f9231d7c05ebd4fbe62d798db5126f5cc5
Jamezjaz <jamezjaz@gmail.com>
2020-08-19 16:44:39 -0500

    Adds sign im errors test

---
+++ spec/Integration/sign_in_errors_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe 'Creates a new account', type: :feature do
+  let(:user) { User.create!(username: 'Jamezjaz', fullname: 'James Chigozie Odufu') }
+  scenario 'Access index page after login' do
+    visit new_session_path
+    fill_in 'username', with: 'Jade'
+    click_button 'Login'
+    expect(page).to have_content('Invalid Credentials')
+  end
+end
